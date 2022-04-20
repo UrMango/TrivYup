e0f5a354e194d47bbdd122102372b8057f947dda
Noam Raz <noamrazbusiness@gmail.com>
2022-04-21 02:48:24 +0300

    Initialize Server

---
+++ Trivia/Trivia.cpp
@@ -0,0 +1,10 @@
+﻿// Trivia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <boost/beast/core.hpp>
+
+int main()
+{
+    
+}
