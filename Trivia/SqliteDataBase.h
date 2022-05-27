ff2568b1d579cb320ebc0e9322c07cbc8238636b
Noam Raz <noamrazbusiness@gmail.com>
2022-05-27 14:37:42 +0300

    Update

--- Trivia/SqliteDataBase.h
+++ Trivia/SqliteDataBase.h
@@ -6,8 +6,12 @@ public:
 	SqliteDataBase();
 	~SqliteDataBase();
 
+	/* V1*/
 	bool doesUserExist(std::string username) override;
 	bool doesPasswordMatch(std::string username, std::string pword) override;
+	void addNewUser(std::string username, std::string pword, std::string email) override;
+
+	/* V2*/
 	void addNewUser(std::string username, std::string pword, std::string email) override;
 private:
 	sqlite3* db;
