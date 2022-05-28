cc2360a43c81749fc5a39600892bdd50940b6e46
shirel hoor <or.shi20@gmail.com>
2022-05-28 13:25:25 +0300

    added struct RoomData

--- Trivia/Room.h
+++ Trivia/Room.h
@@ -1,6 +1,16 @@
 #pragma once
+#include <string>
 
 
+struct RoomData
+{
+	unsigned int id = 0;
+	std::string name = "";
+	unsigned int maxPlayers = 0;
+	unsigned int numOfQuestionsInGame = 0;
+	unsigned int timePerQuestion = 0;
+	unsigned int isActive = 0;
+};
 
 class Room
 {
