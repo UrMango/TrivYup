d209800a9fbad08bda1906811f2199a2f3b2c41d
shirel hoor <or.shi20@gmail.com>
2022-05-28 14:49:33 +0300

    finish

--- Trivia/RoomManager.cpp
+++ Trivia/RoomManager.cpp
@@ -1,6 +1,5 @@
 #include "RoomManager.h"
 
-
 void RoomManager::createRoom(const LoggedUser user, const  RoomData roomData)
 {
 	m_rooms.insert(std::pair<unsigned int, Room&>(_roomID, *(new Room(user, roomData))));
