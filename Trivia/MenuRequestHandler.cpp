1b4c5698e88e8a5590f9565aff26692bd423ba7c
shirel hoor <or.shi20@gmail.com>
2022-05-28 18:55:24 +0300

    added new codes in the enum

--- Trivia/MenuRequestHandler.cpp
+++ Trivia/MenuRequestHandler.cpp
@@ -1,1 +1,26 @@
 #include "MenuRequestHandler.h"
+
+
+MenuRequestHandler::MenuRequestHandler(RequestHandlerFactory& handlerFactory) : m_handlerFactory(handlerFactory){}
+
+MenuRequestHandler::~MenuRequestHandler() {}
+
+bool MenuRequestHandler::isRequestRelevant(const RequestInfo& request) const
+{
+	return (request.msgCode == MT_CLIENT_LOG_IN || request.msgCode == MT_CLIENT_SIGN_UP);
+}
+
+
+RequestResult MenuRequestHandler::handleRequest(const RequestInfo& request) const
+{
+	std::string msg;
+	struct RequestResult result;
+
+	if (!(this->isRequestRelevant(request)))
+	{
+		//insert field to RequestInfo struct
+		result.msg = JsonResponsePacketSerializer::serializeErrorResponse(ErrorResponse("You must first log in or sign up"));
+		result.newHandler = nullptr;
+	}
+	return result;
+}
--- Trivia/MenuRequestHandler.h
+++ Trivia/MenuRequestHandler.h
@@ -2,19 +2,19 @@
 #include "IRequestHandler.h"
 #include "LoggedUser.h"
 #include "RequestHandlerFactory.h"
-
+#include "Helper.h"
 
 class MenuRequestHandler : public IRequestHandler
 {
 private:
-	LoggedUser m_user;
+	//LoggedUser m_user;
 	//RoomManager& m_roomManager;
 	//StatisticsManager& m_statisticsManager;
 	RequestHandlerFactory& m_handlerFactory;
 
 public:
 	MenuRequestHandler(RequestHandlerFactory& handlerFactory);
-	~MenuRequestHandler() {};
+	~MenuRequestHandler();
 
 	virtual bool isRequestRelevant(const RequestInfo& request) const override;
 	virtual RequestResult handleRequest(const RequestInfo& request)const override;
