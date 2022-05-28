787b4ad251101849d6ffe4822bd57bef481bb17a
shirel hoor <or.shi20@gmail.com>
2022-05-28 14:03:52 +0300

    add class RoomManager RoomManager.h

--- Trivia/RoomManager.h
+++ Trivia/RoomManager.h
@@ -2,12 +2,16 @@
 //#include <string>
 //#include "Helper.h"
 //#include <vector>
-//#include "LoggedUser.h"
+#include "Room.h"
+#include <map>
 
 class RoomManager
 {
 public:
-
+	void createRoom(LoggedUser, RoomData);
+	void deleteRoom(int ID);
+	unsigned int getRoomState(int ID);
+	vector<RoomData>getRooms();
 private:
-
+	std::map<unsigned int, Room> m_rooms;
 };
