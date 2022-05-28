2d23df649216fdbce1cf64f370fb53ce8cf8e04c
shirel hoor <or.shi20@gmail.com>
2022-05-28 13:52:10 +0300

    add func remove user in Room.h

--- Trivia/Room.cpp
+++ Trivia/Room.cpp
@@ -9,3 +9,15 @@ void Room::addUser(LoggedUser user)
 {
 	this->m_users.push_back(user);
 }
+
+void Room::removeUser(LoggedUser user)
+{
+	for (int i = 0; i < this->m_users.size(); i++)
+	{
+		if (this->m_users[i].getUsername() == user.getUsername())
+		{
+			this->m_users.erase(this->m_users.begin() + i);
+			break;
+		}
+	}
+}
