ec7e7ce930a94791b8cb89c1d9f0a1775bef62bf
shirel hoor <or.shi20@gmail.com>
2022-05-28 14:21:52 +0300

    add func deleteRoom in RoomManager

--- Trivia/RoomManager.cpp
+++ Trivia/RoomManager.cpp
@@ -4,6 +4,9 @@
 void RoomManager::createRoom(const LoggedUser user, const  RoomData roomData)
 {
 	m_rooms.insert(std::pair<unsigned int, Room&>(_roomID, *(new Room(user, roomData))));
-
 }
 
+void RoomManager::deleteRoom(const int ID)
+{
+	m_rooms.erase(ID);
+}
