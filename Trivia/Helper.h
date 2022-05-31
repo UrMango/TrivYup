121b763babd83ff2461cf6dedf1ac9a6eb4717a2
shirel hoor <or.shi20@gmail.com>
2022-05-31 17:30:10 +0300

    finish

--- Trivia/Helper.h
+++ Trivia/Helper.h
@@ -10,14 +10,16 @@
 using tcp = boost::asio::ip::tcp;
 class IRequestHandler;
 
-enum MessageType 
+enum MessageType
 {
 	MT_CLIENT_LOG_IN = 200,
 	MT_CLIENT_SIGN_UP = 204,
-
-	//MT_CLIENT_FINISH = 207,
-	//MT_CLIENT_EXIT = 208,
-	//MT_SERVER_UPDATE = 101,
+	CREATE_ROOM = 1,
+	GET_ROOMS = 2,
+	GET_PLAYERS_IN_ROOM = 3,
+	JOIN_ROOM = 4,
+	GET_STATISTICS = 5,
+	LOG_OUT = 6,
 };
 
 struct RequestInfo {
