336b69855c1ee4a7059f40d8485876775ddf58a8
Noam Raz <noamrazbusiness@gmail.com>
2022-06-10 02:16:51 +0300

    Added login button functionality

--- Client/trivia-client/src/components/Login/Login.js
+++ Client/trivia-client/src/components/Login/Login.js
@@ -20,8 +20,10 @@ const Login = () => {
 
 	const handleLogin = (e) => {
 		e.preventDefault();
-		dispatch(setUserData({ username }));
-		ws.send(ClientToServerCode.MT_CLIENT_LOG_IN + JSON.stringify({username, password}));
+		if(e.target.id == "yes") {
+			dispatch(setUserData({ username }));
+			ws.send(ClientToServerCode.MT_CLIENT_LOG_IN + JSON.stringify({username, password}));
+		}
 	}
 
 	useEffect(() => {
@@ -35,12 +37,38 @@ const Login = () => {
 			<h2>Log in</h2>
 			<form className="form">
 				<label className="username">Username</label>
-				<input className="usernameInput" type="text" onChange={e => setUsername(e.target.value)} id="username" name="username"/>
+				<input className="usernameInput" type="text" onChange={e => {
+					if(e.target.value.length > 0) {
+						e.target.id = "yes";
+						if(document.getElementsByClassName("passwordInput")[0]?.id == "yes") {
+							document.getElementsByClassName("submitBtn")[0].attributes.getNamedItem("style").textContent = "background-color: rgb(38, 137, 12); cursor: pointer;";
+							document.getElementsByClassName("submitBtn")[0].id = "yes";
+						}
+					} else {
+						e.target.id = "no";
+						document.getElementsByClassName("submitBtn")[0].attributes.getNamedItem("style").textContent = "display: 'block'";
+						document.getElementsByClassName("submitBtn")[0].id = "no";
+					}
+					setUsername(e.target.value);
+				}}/>
 				<br/>
 				<label className="password">Password</label>
-				<input className="passwordInput" type="password" onChange={e => setPassword(e.target.value)} id="pwd" name="pwd"/>
+				<input className="passwordInput" type="password" onChange={e => {
+					if(e.target.value.length > 0) {
+						e.target.id = "yes";
+						if(document.getElementsByClassName("usernameInput")[0]?.id == "yes") {
+							document.getElementsByClassName("submitBtn")[0].attributes.getNamedItem("style").textContent = "background-color: rgb(38, 137, 12); cursor: pointer;";
+							document.getElementsByClassName("submitBtn")[0].id = "yes";
+						}
+					} else {
+						e.target.id = "no";
+						document.getElementsByClassName("submitBtn")[0].attributes.getNamedItem("style").textContent = "display: 'block'";
+						document.getElementsByClassName("submitBtn")[0].id = "no";
+					}
+					setPassword(e.target.value)
+				}} id="pwd" name="pwd"/>
 				<br/>
-				<button className="submitBtn" onClick={handleLogin} type="submit">Log in</button>
+				<button className="submitBtn" onClick={handleLogin} type="submit" style={{display: "block"}}>Log in</button>
 			</form>
 		</div>
 	)
