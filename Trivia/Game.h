6927b2ca2c8dbf29c8230cf3830457bb37f95a49
shirel hoor <or.shi20@gmail.com>
2022-06-15 13:25:09 +0300

    add game.h, game.cpp'

---
+++ Trivia/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include "Helper.h"
+#include <vector>
+#include "LoggedUser.h"
+
+struct GameData
+{
+
+		//+ currentQuestion : Question
+		//+ correctAnswerCount : unsigned int
+
+		//+ wrongAnswerCount : unsigned int
+
+		//+ averangeAnswerTime : unsigned int
+
+
+};
+
+
+
+
+class Game
+{
+public:
+
+
+private:
+
+};
